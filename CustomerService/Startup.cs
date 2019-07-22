65de9a7e3f474993fb150295b5d264643719d8de
viy <vitalii.ievdokymenko@eleks.com>
2019-07-22 17:22:00 +0300

    added swagger

--- CustomerService/Startup.cs
+++ CustomerService/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using CustomerService.Configuration;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CustomerService
 {
@@ -39,6 +40,11 @@ namespace CustomerService
             services.AddScoped<EFUnitOfWork>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "CustomerService API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +58,12 @@ namespace CustomerService
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CustomerService API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
